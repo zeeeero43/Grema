d07c5a6a8437c36f567828dcedd90580bdd543b4
Murat Polat <muratxa@hotmail.de>
2025-07-25 13:09:37 +0200

    vite änderung

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Grema/' : '/',
   plugins: [
     react(),
     runtimeErrorOverlay(),
